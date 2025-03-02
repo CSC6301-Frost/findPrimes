890f7de5bc16b3382db01963d81f77c7adc22ee5
CSC6301-Frost <frostj@merrimack.edu>
2025-03-02 08:18:52 -0500

    Code Documentation Added
    - Python documentation added to the finPrimes.py file itself.
    - README.md file created.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# findPrimes
-CSC 6301 Week 7 Assignment
+# CSC-6301 Find Primes Programm
+
+This Python application generates a random number n specified number of times and determines whether it is a prime number or not.  The Python tool cProfile is used to profile the program's execution
+
+---
+
+## About
+
+The **FindPrimes** program is part of CSC-6301 Assignment 7.  The initial program is ineffecient by design and can be found in Google Drive here:  https://drive.google.com/file/d/1w52JoS27f7qMv9I_JCqrTvUiiKQvJ-Fm/view.  Analyze using cProfile to find which function should be changed to make it faster.  Improve the function to make it faster and confirm that it is faster by rerunning cProfile.
+
+---
+
+### Running the Program
+
+After compiling, navigate to the "lib" subfolder and execute the program with the following in the command line / terminal of choice:
+
+```bash
+python finPrimes.py
+```
+Once running, the follow the instructions in the terminal.
+
+## Author
+
+**Jeffrey Frost**
+
+- **Version:** 1.0 (Homework Assignment 7)
+- **Date:** 02MAR25
+
+---
--- finPrimes.py
+++ finPrimes.py
@@ -1,9 +1,29 @@
+""" CSC 6301 - Assignment 7
+Jeffrey Frost
+Version 1.0
+"""
+
+
 import random
 
 def guess():
+    """guess function generates a random integer starting from 2 to 5000
+
+    Returns:
+        int: random integer from 2 to 5000
+    """
     return random.randint(2, 5000)
 
 def isPrime(x):
+    """isPrime function determine whether the argument x is a prime number or not.  x must be an integer
+    to avoid a typeError at the range function.
+
+    Args:
+        x (int): candidate value to determine whether it is a prime number or not
+
+    Returns:
+        boolean: True if prime number / False if not prime number
+    """
     for i in range(x):
         for j in range(x):
             if i * j == x:
@@ -11,6 +31,15 @@ def isPrime(x):
     return True
 
 def findPrimes(num):
+    """findPrimes function chooses a random number, which it then determines is a prime number or not.  
+    This effort is repeated for as many times as the argument num specifies.
+
+    Args:
+        num (int): specifies the amount of random numbers to generate
+
+    Returns:
+        int[]: list of the prime numbers found
+    """
     primes = []
     for i in range(num):
         p = guess()
